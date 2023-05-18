f0be4bf52b7c1579c057571e961f20dcc7c8ab55
Alex Bernt <alexbernt100@gmail.com>
2023-05-17 23:19:59 -0500

    Render task text

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,4 +1,10 @@
-const TodoList = () => {
+import { ITask } from "@/types/tasks";
+
+interface TodoListProps {
+  tasks: ITask[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
     <div className="overflow-x-auto">
       <table className="table w-full">
@@ -10,11 +16,12 @@ const TodoList = () => {
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          <tr>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-          </tr>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.text}</td>
+              <td>Quality Control Specialist</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
