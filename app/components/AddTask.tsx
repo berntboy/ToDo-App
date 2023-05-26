6bfe9aea9e2058571dc061e6f12c40bf7b1fd5a2
Alex Bernt <alexbernt100@gmail.com>
2023-05-25 23:22:29 -0500

    Add modal logic

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,14 +1,21 @@
+"use client";
+
+import { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import Modal from "./Modal";
 
 const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
   return (
     <div>
-      <button className="btn btn-prrimary w-full">
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-prrimary w-full"
+      >
         Add new task <AiOutlinePlus className="ml-2" size={18} />
       </button>
-
-      <Modal />
+      <Modal modalOpen={modalOpen} />
     </div>
   );
 };
