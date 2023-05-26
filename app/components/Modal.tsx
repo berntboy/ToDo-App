eef81aa79a820e8ffb6ad2c244f2825ab83d2f3c
Alex Bernt <alexbernt100@gmail.com>
2023-05-25 23:22:49 -0500

    Add Modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,6 +1,10 @@
-const Modal = () => {
+interface ModalProps {
+  modalOpen: boolean;
+}
+
+const Modal: React.FC<ModalProps> = ({ modalOpen }) => {
   return (
-    <div className="modal modal-open">
+    <div className={`modal ${modalOpen ? "modal-open" : ""}`}>
       <div className="modal-box relative">
         <label
           htmlFor="my-modal-3"
@@ -12,7 +16,7 @@ const Modal = () => {
           Congratulations random Internet user!
         </h3>
         <p className="py-4">
-          You've been selected for a chance to get one year of subscription to
+          Youve been selected for a chance to get one year of subscription to
           use Wikipedia for free!
         </p>
       </div>
