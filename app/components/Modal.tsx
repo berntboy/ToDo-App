dff86c3526153ac9a2dfb1d1c8352c4ed5bbb60a
Alex Bernt <alexbernt100@gmail.com>
2023-05-28 23:49:11 -0500

    Add on click close modal feature

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,13 +1,14 @@
 interface ModalProps {
   modalOpen: boolean;
+  setModalOpen: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ modalOpen }) => {
+const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen }) => {
   return (
     <div className={`modal ${modalOpen ? "modal-open" : ""}`}>
       <div className="modal-box relative">
         <label
-          htmlFor="my-modal-3"
+          onClick={() => setModalOpen(false)}
           className="btn btn-sm btn-circle absolute right-2 top-2"
         >
           ✕
