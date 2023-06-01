ed4723fb608974d35c78d31aeec309cb53890072
Alex Bernt <alexbernt100@gmail.com>
2023-06-01 10:56:19 -0500

    Change base Url

--- api.ts
+++ api.ts
@@ -1,6 +1,6 @@
 import { ITask } from "./types/tasks";
 
-const baseUrl = "http://localhost:3001/tasks";
+const baseUrl = "http://localhost:3001";
 
 export const getAllTodos = async (): Promise<ITask[]> => {
   const res = await fetch(`${baseUrl}/tasks`);
