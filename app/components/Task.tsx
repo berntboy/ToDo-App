eed6747d875ee087a51652164a68dbb0ff45c394
Alex Bernt <alexbernt100@gmail.com>
2023-06-15 22:34:39 -0500

    add edit icon

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,4 +1,5 @@
 import { ITask } from "@/types/tasks";
+import { FiEdit } from "react-icons/fi";
 
 interface TaskProps {
   task: ITask;
@@ -8,7 +9,9 @@ const Task: React.FC<TaskProps> = ({ task }) => {
   return (
     <tr key={task.id}>
       <td>{task.text}</td>
-      <td>Blue</td>
+      <td>
+        <FiEdit />
+      </td>
     </tr>
   );
 };
